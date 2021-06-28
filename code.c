0aba148af7365518c69bc8885bcb4057f41c2a8e
AmrMoh <82750474+AmrMoh0325@users.noreply.github.com>
2021-06-28 14:19:24 +0200

    Resolved conflict

--- code.c
+++ code.c
@@ -7,16 +7,15 @@ int main()
 	return 0;
 }
 
-<<<<<<< HEAD
 //feature 1 code
 int multiply (int x, int y)
 {
 	return x*y;
 }
-=======
+
 //feature2
 int xor_num(int n, int m)
 {
 	return n^m;
 }
->>>>>>> main
+
